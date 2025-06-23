281816b32e6b90a16dd561c5245e4195a982144f
Robiul <robiulhossain0110@gmail.com>
2025-06-23 09:58:13 +0600

    New Array

---
+++ src/main/java/com/methods/methodexample/MinMaxArray.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ *
+ * @author A-3
+ */
+public class MinMaxArray {
+    public static void main(String[] args) {
+        MinMax();
+        SumAVG();
+        SortArray();
+    }
+    
+    
+    
+    public static void MinMax(){
+        int [] maxs = {9,5,6,7,5,7,3};
+        
+        int max = maxs[0];
+        int min = maxs[0];
+        for(int i = 0; i < maxs.length; i++){
+            if(max < maxs[i]){
+                max = maxs[i];
+            }
+            if(min > maxs[i]){
+                min = maxs[i];
+            }
+        }
+        System.out.println(" Max " + max + " Min " + min);
+    }
+    
+    // Calculate the sum and average of all elements in array
+    
+    public static void SumAVG(){
+        int [] array1 = {9,5,6,7,5,7,3,85};
+        
+        int total = 0;
+        
+        for(int i = 0; i < array1.length; i++){
+            
+            total +=  array1[i];
+            
+           
+        }
+        double averages = (double) total / array1.length;
+         System.out.println(" Sum " + total + " Average: " + averages);
+    }
+    
+    
+    
+    //Sorting Array Ascending Order
+    
+    public static void SortArray(){
+        int [] sorting = {6,9,8,7,3,2,5,4,1};
+        
+        Arrays.sort(sorting);
+        for(int i = 0; i < sorting.length; i++){
+            
+           
+        }
+        System.out.println(Arrays.toString(sorting));
+    }
+}
