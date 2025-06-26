211d89ffe17a98e8d20c9b9d799ed2ebac190def
Robiul <robiulhossain0110@gmail.com>
2025-06-26 13:02:16 +0600

    Methods

---
+++ src/main/java/com/methods/methodexample/ObjectMethods/BMW.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample.ObjectMethods;
+
+/**
+ *
+ * @author A-3
+ */
+public class BMW {
+    String brand;
+    String color;
+    double speed;
+    
+
+
+    public BMW(String brand, String color, double speed) {
+        this.brand = brand;
+        this.color = color;
+        this.speed = speed;
+    }
+
+    public BMW() {
+        
+    }
+    
+    
+}
