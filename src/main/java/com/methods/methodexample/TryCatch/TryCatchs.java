3bf5aefd85bad1a773bc36ade867a06eb3ef23d0
Robiul <robiulhossain0110@gmail.com>
2025-06-30 10:30:41 +0600

    TryCatch

---
+++ src/main/java/com/methods/methodexample/TryCatch/TryCatchs.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample.TryCatch;
+
+/**
+ *
+ * @author A-3
+ */
+public class TryCatchs {
+    public static void main(String[] args) {
+        tryCatchExample(10,0);
+    }
+    
+    static void tryCatchExample(int a, int b){
+        try{
+            int results = a/b;
+            System.out.println("Results: " + results);
+        } catch(ArithmeticException e){
+            System.out.println("ArithmeticException" + e.getMessage());
+        }
+    }
+}
