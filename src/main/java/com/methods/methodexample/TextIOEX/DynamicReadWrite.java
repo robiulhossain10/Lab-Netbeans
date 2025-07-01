fc85cbd40d3987a46ec6fb73724310b42859790f
Robiul <robiulhossain0110@gmail.com>
2025-07-01 13:30:14 +0600

    Text I/O

---
+++ src/main/java/com/methods/methodexample/TextIOEX/DynamicReadWrite.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample.TextIOEX;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author A-3
+ */
+public class DynamicReadWrite {
+    
+    public static void main(String[] args) {
+        
+        try {
+            
+            FileReader reading = new FileReader("C:\\Users\\A-3\\Desktop\\reads.txt");
+            FileWriter write = new FileWriter("C:\\Users\\A-3\\Desktop\\reads2.txt");
+            
+            int Character ;
+            
+            
+            while((Character = reading.read()) != -1){
+            
+                write.write((char)Character);
+            
+            
+            
+            }
+            write.close();
+            
+            
+            
+            
+            
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        
+        
+        
+        
+        
+    }
+    
+}
