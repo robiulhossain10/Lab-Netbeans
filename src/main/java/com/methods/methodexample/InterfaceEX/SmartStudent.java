abada0739a4c099cfaa5158761fc064d552f72d4
Robiul <robiulhossain0110@gmail.com>
2025-07-02 12:49:07 +0600

    Interface

---
+++ src/main/java/com/methods/methodexample/InterfaceEX/SmartStudent.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample.InterfaceEX;
+
+/**
+ *
+ * @author A-3
+ */
+class SmartStudent extends Student implements Human {
+    // Human interface এর method গুলো
+    public void eat() {
+        System.out.println("I Eat");
+    }
+
+    public void sleep() {
+        System.out.println("I Sleep");
+    }
+
+    // Student abstract class এর method
+    public void study() {
+        System.out.println("I Read in Book");
+    }
+}
+
