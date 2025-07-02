fe5e5c1329c65da1399fce7ad04336294c9cfe6b
Robiul <robiulhossain0110@gmail.com>
2025-07-02 12:48:29 +0600

    Interface

---
+++ src/main/java/com/methods/methodexample/InterfaceEX/Human.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.methods.methodexample.InterfaceEX;
+
+/**
+ *
+ * @author A-3
+ */
+public interface Human {
+     void eat();
+    void sleep();
+}
