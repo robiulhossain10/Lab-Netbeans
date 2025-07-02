9d0bbf7b5c1e99a55cb57b57fe64f5098af571e4
Robiul <robiulhossain0110@gmail.com>
2025-07-02 12:48:40 +0600

    Interface

---
+++ src/main/java/com/methods/methodexample/InterfaceEX/Pakna.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample.InterfaceEX;
+
+/**
+ *
+ * @author A-3
+ */
+abstract class Pakna {
+    abstract void fly();
+
+    void wings() {
+        System.out.println("Amar Dana Ace");
+    }
+}
+
