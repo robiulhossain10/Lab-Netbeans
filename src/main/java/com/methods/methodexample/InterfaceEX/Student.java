9cc51e8a9e208280a5dabb87610ef1265bdef524
Robiul <robiulhossain0110@gmail.com>
2025-07-02 12:48:51 +0600

    Interface

---
+++ src/main/java/com/methods/methodexample/InterfaceEX/Student.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample.InterfaceEX;
+
+/**
+ *
+ * @author A-3
+ */
+abstract class Student {
+    abstract void study();
+
+    void attendClass() {
+        System.out.println("I am go to school");
+    }
+}
+
