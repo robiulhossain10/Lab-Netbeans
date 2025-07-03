9bd5959f16294a9e1e54a782ab660f5ecba046e4
Robiul <robiulhossain0110@gmail.com>
2025-07-03 09:56:15 +0600

    interface

---
+++ src/main/java/com/methods/methodexample/InterfaceEX/Flyable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.methods.methodexample.InterfaceEX;
+
+/**
+ *
+ * @author A-3
+ */
+public interface Flyable {
+    void fly();
+}
