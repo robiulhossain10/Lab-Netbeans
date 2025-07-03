eb1b1cbbd917caeff44c0243bc2965f0cd6e04b3
Robiul <robiulhossain0110@gmail.com>
2025-07-03 09:56:44 +0600

    interface

---
+++ src/main/java/com/methods/methodexample/InterfaceEX/AnimalMain.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample.InterfaceEX;
+
+/**
+ *
+ * @author A-3
+ */
+public class AnimalMain {
+    public static void main(String[] args) {
+        Bird br = new Bird("Sparrow");
+        
+        br.fly();
+        br.sound();
+        br.read();
+    }
+}
