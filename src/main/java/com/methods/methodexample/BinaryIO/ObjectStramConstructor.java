d064b2d300fa730d6b61518983402af2a07c2ed2
Robiul <robiulhossain0110@gmail.com>
2025-07-05 13:09:24 +0600

    Buffered stream

---
+++ src/main/java/com/methods/methodexample/BinaryIO/ObjectStramConstructor.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.methods.methodexample.BinaryIO;
+
+/**
+ *
+ * @author A-3
+ */
+public class ObjectStramConstructor {
+    private String name;
+    private int age;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public ObjectStramConstructor(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+    
+    
+}
