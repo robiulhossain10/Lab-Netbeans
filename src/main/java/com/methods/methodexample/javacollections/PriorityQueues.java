0dc65cbb5168788b16f63e0cc8e683b7c9f5d9d1
Robiul <robiulhossain0110@gmail.com>
2025-07-08 09:47:17 +0600

    Priority Queue

--- src/main/java/com/methods/methodexample/javacollections/PriorityQueues.java
+++ src/main/java/com/methods/methodexample/javacollections/PriorityQueues.java
@@ -4,12 +4,22 @@
  */
 package com.methods.methodexample.javacollections;
 
+import java.util.PriorityQueue;
+
 /**
  *
  * @author A-3
  */
 public class PriorityQueues {
     public static void main(String[] args) {
+        PriorityQueue<String> pq = new PriorityQueue<>();
         
+        pq.add("BMW");
+        pq.add("Audi");
+        pq.add("BYD");
+        pq.add("Tesla");
+        
+        
+        System.out.println("Car: " + pq.poll());
     }
 }
